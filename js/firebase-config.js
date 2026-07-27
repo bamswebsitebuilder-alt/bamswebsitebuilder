cfb72f9d04f190a968aba9eec41e42f517984a65
bamswebsitebuilder-alt <bamswebsitebuilder@gmail.com>
2026-07-27 01:53:16 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB68Oh9_mBiWOXAJjF1R77-4eALwahkbXA",
@@ -11,8 +15,13 @@ const firebaseConfig = {
   appId: "1:185476076163:web:29a1003fa165f6c7e781b6"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Services
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { app, auth, db };
+// Export services
+export { app, auth, db, storage };
